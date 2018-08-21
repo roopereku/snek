38585f717d981d6e3a6d52cf20d5e73f147ddcf9
Geckospaniel <the2mappers@gmail.com>
2018-08-22 01:55:22 +0300

    Added some comments

--- Config.cpp
+++ Config.cpp
@@ -12,9 +12,47 @@ T strTo(std::string str)
 	return value;
 }
 
+Config::Config(int argc, char** argv)
+{
+	// All valid arguments are declared here.
+
+	configDataMultiple["-sensitivity"].resize(4, 2.5f);
+
+	configDataSingle["-appledelay"] = 20.0f;
+	configDataSingle["-players"] = 1;
+	configDataSingle["-maxapples"] = 1;
+
+	if(argc <= 1)
+	{
+		// Parse the default config file.
+
+		const char* defaultPath = "conf";
+		SDL_Log("No arguments passed! Loading from %s", defaultPath);
+		parseFile(defaultPath);
+	}
+
+	else if(argc >= 2)
+	{
+		// Parse command-line arguments.
+
+		SDL_Log("%d arguments passed!", argc - 1);
+
+		// Argv is put into a vector to simplify things later on.
+		std::vector <std::string> args(argc);
+		for(size_t i = 0; i < args.size(); i++)
+			args[i] = argv[i];
+
+		parseArguments(args);
+	}
+}
+
 void Config::parseFile(const char* path)
 {
-	std::vector <std::string> data(1);
+	std::vector <std::string> data;
+
+	/*	Open the config file and put the arguments in data.
+	 *	If the file couldn't be opened, notify the user.
+	 */
 
 	std::ifstream fileI(path);
 	if(fileI.is_open())
@@ -24,13 +62,17 @@ void Config::parseFile(const char* path)
 
 		while(std::getline(fileI, line))
 		{
+			/*	Loop through line.
+			 *	If a space or the end of the line occurs,
+			 *	push arg in data.
+			 */
+
 			line+=' ';
 			for(size_t i = 0; i < line.length(); i++)
 			{
 				if(line[i] != ' ')
 				{
 					arg+=line[i];
-					SDL_Log("%c", line[i]);
 				}
 				else
 				{
@@ -51,39 +93,19 @@ void Config::parseFile(const char* path)
 		SDL_Log("Unable to open %s!", path);
 }
 
-Config::Config(int argc, char** argv)
-{
-	configDataMultiple["-sensitivity"].resize(4, 2.5f);
-
-	configDataSingle["-appledelay"] = 20.0f;
-	configDataSingle["-players"] = 1;
-	configDataSingle["-maxapples"] = 1;
-
-	if(argc <= 1)
-	{
-		const char* defaultPath = "conf";
-		SDL_Log("No arguments passed! Loading from %s", defaultPath);
-		parseFile(defaultPath);
-	}
-
-	else if(argc >= 2)
-	{
-		SDL_Log("%d arguments passed!", argc - 1);
-
-		std::vector <std::string> args(argc);
-		for(size_t i = 0; i < args.size(); i++)
-			args[i] = argv[i];
-
-		parseArguments(args);
-	}
-}
-
 void Config::parseArguments(std::vector <std::string> args)
 {
-	SDL_Log("%d arguments found!", (int)args.size());
+	SDL_Log("%d arguments found!", (int)args.size() - 1);
 
-	for(size_t i = 1; i < args.size(); i++)
+	for(size_t i = 0; i < args.size(); i++)
 	{
+		/*	Currently the game can crash if given arguments are
+		 *	for ex. -players -sensitivity 1
+		 *	Because the config parser assumes that the syntax to be correct.
+		 */
+
+		// Check if argument is found in configDataSingle.
+
 		if(configDataSingle.find(args[i]) != configDataSingle.end())
 		{
 			std::string key = args[i];
@@ -94,6 +116,8 @@ void Config::parseArguments(std::vector <std::string> args)
 			float value = configDataSingle[ args[i - 1] ];
 			SDL_Log("%s set to %.2f!", key.c_str(), value);
 		}
+		
+		// Check if argument is found in configDataMultiple.
 
 		else if(configDataMultiple.find(args[i]) != configDataMultiple.end())
 		{
@@ -104,12 +128,14 @@ void Config::parseArguments(std::vector <std::string> args)
 			i++;
 			float value = strTo<float>(args[i]);
 
+			// If index x doesn't exist in key z, push a new element in z
 			if(playerIndex >= configDataMultiple[key].size())
 			{
 				SDL_Log("Pushed %.2f to %s at %d!", value, key.c_str(), (int)playerIndex);
 				configDataMultiple[key].push_back(value);
 			}
 
+			// If index x exists in key z, modify value at index x
 			else
 			{
 				SDL_Log("Modified index %d of %s!", (int)playerIndex - 1, key.c_str());
@@ -119,11 +145,15 @@ void Config::parseArguments(std::vector <std::string> args)
 
 		else
 		{
-			
 			SDL_Log("'%s' is not a valid argument!", args[i].c_str());
 		}
 	}
 }
+
+/*	TODO
+ *	add a check to see if the indices are in range
+ *	of the configuration data.
+ */
 
 float Config::fromSingle(std::string index)
 {
--- Config.h
+++ Config.h
@@ -12,12 +12,22 @@ class Config
 public:
 	Config(int argc, char** argv);
 
+	// Configuration data getters
 	float fromSingle(std::string index);
 	float fromMulti(std::string index, size_t i);
 
 private:
 	void parseArguments(std::vector <std::string> args);
 	void parseFile(const char* path);
+
+	/*	The configuration data is stored in 2 maps.
+	 *
+	 *	The first one contains things such as -maxapples and -players
+	 *	which contain a single value.
+	 *
+	 *	The second one contains things such as -sensitivity which requires
+	 *	2 values (ex. -sensitivity playerIndex value).
+	 */
 
 	std::map <std::string, float> configDataSingle;
 	std::map <std::string, std::vector <float>> configDataMultiple;
